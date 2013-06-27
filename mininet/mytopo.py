8b5f2ad8a5909d7be50e431a380eb6a195f9ce6b
masnec <devnull@localhost>
2013-06-27 07:58:11 -0700

    add mininet

---
+++ mininet/mytopo.py
@@ -0,0 +1,58 @@
+"""
+Run: sudo mn --custom mytopo.py --topo mytopo --switch ovsk --controller remote --mac --link tc
+Topo: 
+                 -- h2
+          |-- s2 |
+c1 -- s1 -|      -- h3
+          |-- s3 -- h4
+"""
+
+import sys, os, json
+from mininet.topo import Topo
+
+def LoadTopo(file_name):
+   # Read file
+   file = open(file_name, "r")
+   json_str = ''
+   # Get data
+   while True:
+      str = file.readline()
+      if len(str) == 0:
+         file.close()
+         break
+      json_str += str.replace('\n', '').strip()
+   # Load JSON 
+   topo = json.loads(json_str)
+   return topo
+
+class MyTopo( Topo ):
+    
+    def __init__( self ):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.__init__( self )
+
+        # Load topology from file
+        topo_data = LoadTopo("/home/ubuntu/mininet/custom/topo.json")
+        # Add hosts and switches
+        node = {}
+        for name in topo_data["node"]:
+            if name[0] == 's':
+                # Switch
+                node[name] = self.addSwitch( str(name) )
+            elif name[0] in {'c', 'h'}:
+                # Camara or Host
+                node[name] = self.addHost( str(name) )
+        
+        # Add links
+        for link_obj in topo_data["link"]:
+           # Data mapping
+           src = node[ link_obj["src"] ]
+           dst = node[ link_obj["dst"] ]
+           linkopts = dict(bw=link_obj["bw"], delay=link_obj["delay"])
+           # Set link
+           self.addLink( src, dst, **linkopts )
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
+
